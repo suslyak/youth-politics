cb6d4ac8ed122db54443cf80511a2ba30c5942a2
Suslyak <sergey.suslyak@mail.ru>
2021-12-12 18:42:40 +0300

    initial commit with configs and some initial styles

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router as BrowserRouter} from 'react-router-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from "redux-thunk";
+import {composeWithDevTools} from 'redux-devtools-extension';
+import App from './components/App/App';
+import browserHistory from "./browser-history";
+
+import './sass/style.scss';
+
+
+ReactDOM.render(
+      <BrowserRouter history={browserHistory}>
+        <App
+        />
+      </BrowserRouter>,
+    document.querySelector(`#root`)
+);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        open: false,
+        port: 1337,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+            loader: 'babel-loader',
+
+            },
+        },
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: ['css-loader', 'sass-loader']
+            })
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: ['file-loader'],
+        }
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devtool: 'source-map',
+    plugins: [
+        new ExtractTextPlugin('./css/main.css'),
+     ]
+};
